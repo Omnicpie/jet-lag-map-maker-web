06c9736a8f0610aa716eb5dd319818074aa443ba
Ry Anderson <60347977+Omnicpie@users.noreply.github.com>
2026-04-14 17:48:50 +0100

    Add generating page skeleton (no progress bar)

--- src/pages/Generating/Generating.tsx
+++ src/pages/Generating/Generating.tsx
@@ -1,18 +1,43 @@
+import { useCallback, useEffect, useState } from "react";
 import type { Tab } from "../../types/Tab";
+import "./Generating.css";
 
 type GeneratingProps = {
   setCurrentTab: React.Dispatch<React.SetStateAction<Tab>>;
 };
 
 const Generating = ({ setCurrentTab }: GeneratingProps) => {
-  const progressPhase = () => {
+  const [progress, setProgress] = useState(0);
+
+  const progressPhase = useCallback(() => {
     setCurrentTab("confirm");
+  }, [setCurrentTab]);
+
+  const cancel = () => {
+    setCurrentTab("new");
   };
 
+  useEffect(() => {
+    const int = setInterval(() => {
+      setProgress((p) => p + 1);
+    }, 150);
+
+    return () => {
+      clearInterval(int);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (progress >= 100) {
+      progressPhase();
+    }
+  }, [progress, progressPhase]);
+
   return (
-    <div>
-      Generating
-      <button onClick={progressPhase}>Next Phase</button>
+    <div className="generating-page">
+      <h1>Generating...</h1>
+      <div className="progress-bar">{progress}%</div>
+      <button onClick={cancel}>Cancel</button>
     </div>
   );
 };
