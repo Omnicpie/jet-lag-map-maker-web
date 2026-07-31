c5aa9ba03be24bd37806510708cdcfaae6c0fa3c
Ry Anderson <60347977+Omnicpie@users.noreply.github.com>
2026-07-31 19:00:03 +0100

    Fix train_stations omitted from nominatim results

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,2 +1,6 @@
 export const GOOGLE_STATION_TYPES = ["train_station", "transit_station"];
-export const NOMINATIM_STATION_TYPES = ["station", "railway_station"];
+export const NOMINATIM_STATION_TYPES = [
+  "station",
+  "railway_station",
+  "train_station",
+];
